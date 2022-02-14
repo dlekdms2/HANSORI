cfd1ca843d8d4017334ea409010f897eb28f3872
your_name <dlekdms_02@naver.com>
2022-02-14 18:52:23 +0900

    lde02

---
+++ lde02.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const style = {
+    color: 'black',
+    fontSize: 30,
+}
+function daeun(){
+    return (
+        <div
+        className="contents"
+        style={{hight: "100vh", paddingTop: "50vh", backgroundColor: "white"}}>
+            제발 돼라<br />
+            <b style={style}>젭알!!!!!!!!!</b>
+
+        </div>
+    );
+}
+export default daeun;
